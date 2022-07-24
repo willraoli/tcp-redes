6571c8bcdf45eecfce21ecb5a1bf0df1d0767f55
Rafael Azevedo Lezama <rafa11al@hotmail.com>
2022-07-24 20:17:22 -0300

    elevador agora para randomicamente e notifica estar parado

--- client.py
+++ client.py
@@ -1,24 +1,59 @@
 import socket
+import random
+import time
 
-if __name__ == '__main__':
-  ip = '0.0.0.0'
-  port = 8000
+MAX_LEVEL = 20
 
+def notifyTechnician(id):
   server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   server.connect((ip, port))
 
-  while True:
-    try:
-      string = input('Manda a msg ae dudão\n')
-      
-      if string != 'quit':
-        server.send(bytes(string, 'utf-8'))
-        buffer = server.recv(2048)
-        buffer = buffer.decode('utf-8')
-        print('Recebido:', buffer)
-      else:
-        server.close()
-        break
-    except:
-      print('zuou')
+  server.send(bytes('[' + str(id) + '] NOK', 'utf-8'))
+  buffer = server.recv(2048)
+  buffer = buffer.decode('utf-8')
+  if(buffer != '' or buffer != null):
+    print(buffer)  
+  else:
+   print('vazio')
+  server.close()
+    
+
+
+def moveElevator():
+  stops = random.randint(2,19)
+  for i in range(1, MAX_LEVEL):
+    print("Subindo para o andar " + str(i))
+    time.sleep(1)
+    if (i == stops):
+      print('Elevador está com problemas')
+      notifyTechnician(1000)
       break
+
+if __name__ == '__main__':
+  ip = '192.168.1.100'
+  port = 5000
+
+  moveElevator()
+
+  # server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  # server.connect((ip, port))
+
+  # while True:
+  #   try:
+  #     # string = input('Manda a msg ae dudão\n')
+      
+  #     if string != 'quit':
+  #       server.send(bytes(string, 'utf-8'))
+  #       buffer = server.recv(2048)
+  #       buffer = buffer.decode('utf-8')
+  #       print('Recebido:', buffer)
+  #     else:
+  #       server.close()
+  #       print('conexão com servidor fechada')
+  #       break
+  #   except Exception as e:
+  #     print('zuou')
+  #     print(e)
+  #     break
+
+  
--- server.py
+++ server.py
@@ -1,24 +1,41 @@
+from ast import Try
 import socket
 from _thread import *
 
 if __name__ == '__main__':
-  ip = '0.0.0.0'
-  port = 8000
+  ip = '192.168.1.100'
+  port = 5000
+  clients = []	
 
-  # TCP = SOCK_STREAM, UDP = SOCK_DGRAM
-  # AF_INET = IPv4
   server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   server.bind((ip, port))
-  server.listen(5)
+  server.listen(1)
+
+  def notifyTech(conn, string):
+    for client in clients:
+      if (client != client):
+        try:
+          client.send(bytes(string, 'utf-8'))
+        except:
+          print("não enviou")
 
   def newClient(conn, address):
     while True:
       string = client.recv(2048)
       string = string.decode('utf-8')
-      conn.send(bytes(string, 'utf-8'))
-      
+      if(string.find('NOK') or string.find('OK')):
+        notifyTech(conn, string)
+        aux = string[:6] + ' OK'
+        conn.send(bytes(aux, 'utf-8'))
+
+      # try:
+      #   conn.send(bytes(string, 'utf-8'))
+      # except:
+      #   print("não conectou")
+
   while True:
     client, address = server.accept()
+    clients.append(client)
     start_new_thread(newClient, (client, address))
     print('Client connected from:', address[0], ':', address[1])
 
